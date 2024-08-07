5d48e378047b7bda2f7f4828393ea86ba3e24d95
Popcat <liam.ahlo@multi.fi>
2024-08-07 16:07:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConversionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What do you want to convert?");
+            string input = Console.ReadLine();
+
+
+
+        }
+    }
+}
