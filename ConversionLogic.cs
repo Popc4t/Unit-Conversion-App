cf3a042c531a01dce701e225799867a1734d289a
Popcat <liam.ahlo@multi.fi>
2024-08-15 20:12:58 +0300

    decided to rewrite the project and split it in different files

---
+++ ConversionLogic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConversionApp
+{
+    public class Metric // Converting metric units to imperial.
+    {
+        public static void ToFahrenehit()
+        {
+            double input = Convert.ToDouble(Console.ReadLine());
+            double fahrenheit = input * 5 / 9 + 32;
+            Console.WriteLine(input + "°C converted to Fahrenheit = " + fahrenheit + "°F");
+            
+
+
+
+        }
+
+
+    }
+}
