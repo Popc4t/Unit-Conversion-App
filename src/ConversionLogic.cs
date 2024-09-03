539b40fcc50ba043af64a01c8231f620aa3e10f1
Popcat <liam.ahlo@multi.fi>
2024-09-03 18:06:57 +0300

    Finished the project up.

---
+++ src/ConversionLogic.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace UnitConversionApp
+{
+    public static class Metric // Converting metric units to imperial.
+    {
+        
+        public static void ToFahrenheit()
+        {
+            Console.WriteLine("\nInput a value in Celsius to convert to Fahrenheit:");
+            double celsiusinput = Convert.ToDouble(Console.ReadLine());
+            double fahrenheit = celsiusinput * 9 / 5 + 32;
+            Console.WriteLine("\n" + celsiusinput + "°C converted to Fahrenheit = " + fahrenheit + "°F");
+            MainMenu.Controls();
+        }
+        public static void ToPounds()
+        {
+            Console.WriteLine("\nInput a value in kilograms to convert to pounds:");
+            double kiloinput = Convert.ToDouble(Console.ReadLine());
+            double pounds = kiloinput * 2.204623;
+            Console.WriteLine("\n" + kiloinput + "kg converted to pounds = " + pounds + " lbs");
+            MainMenu.Controls();
+        }
+        public static void ToFeet()
+        {
+            Console.WriteLine("\nInput a value in meters to convert to feet:");
+            double meterinput = Convert.ToDouble(Console.ReadLine());
+            double feet = meterinput * 3.28084;
+            Console.WriteLine("\n" + meterinput + " meters converted to feet = " + feet + " ft");
+            MainMenu.Controls();
+        }
+    }
+    public static class Imperial
+    {
+        public static void ToCelsius()
+        {
+            Console.WriteLine("\nInput a value in Fahrenheit to convert to Celsius: ");
+            double fahrenheitinput = Convert.ToDouble(Console.ReadLine());
+            double celsius = (fahrenheitinput - 32) * 5 / 9;
+            Console.WriteLine("\n" + fahrenheitinput + "°F converted to Celsius = " + celsius + "°C");
+            MainMenu.Controls();
+        }
+        public static void ToKilograms()
+        {
+            Console.WriteLine("\nInput a value in pounds to convert to kilograms:");
+            double poundinput = Convert.ToDouble(Console.ReadLine());
+            double kilograms = poundinput * 0.453592;
+            Console.WriteLine("\n" + poundinput + " lbs converted to kilograms = " + kilograms + " kg");
+            MainMenu.Controls();
+        }
+        public static void ToMeters()
+        {
+            Console.WriteLine("\nInput a value in feet to convert to meters:");
+            double ftinput = Convert.ToDouble(Console.ReadLine());
+            double meters = ftinput * 0.3048;
+            Console.WriteLine("\n" + ftinput + " meters converted to feet = " + meters + " ft");
+            MainMenu.Controls();
+        }
+
+    }
+
+}
---
+++ src/mainprogram.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace UnitConversionApp
+{
+    public static class MainMenu
+    {
+        
+
+        public static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("\nInput 'M' to convert metric units to imperial.");
+            Console.WriteLine("Input 'I' to convert imperial units to metric.");
+            ConsoleKeyInfo keyinfo = Console.ReadKey();
+            switch (keyinfo.Key)
+            {
+                case ConsoleKey.M:
+                    MetricControls();
+                    break;
+                case ConsoleKey.I:
+                    ImperialControls();
+                    break;
+            }   
+
+        }
+        public static void Controls()
+        {
+            Console.WriteLine("\nInput 'D' to return to the main menu.");
+            Console.WriteLine("Input 'Q' to quit.");
+            ConsoleKeyInfo keypressed = Console.ReadKey();
+
+            switch (keypressed.Key)
+            {
+                case ConsoleKey.D:
+                    Main();
+                    break;
+                case ConsoleKey.Q:
+                    Environment.Exit(0);
+                    break;
+
+
+            }
+
+        }
+        
+
+        public static void MetricControls()
+        {
+            Console.WriteLine("\nPress 'C' to convert Celsius to Fahrenheit.");
+            Console.WriteLine("Press 'K' to convert kilograms to pounds.");
+            Console.WriteLine("Press 'M' to convert meters to feet.");
+
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            switch (key.Key)
+            {
+                case ConsoleKey.C:
+                    Metric.ToFahrenheit();
+                    break;
+                case ConsoleKey.K:
+                    Metric.ToPounds();
+                    break;
+                case ConsoleKey.M:
+                    Metric.ToFeet();
+                    break;
+            }
+
+        }
+        public static void ImperialControls()
+        {
+            Console.WriteLine("\nPress 'F' to convert Fahrenheit to Celsius.");
+            Console.WriteLine("Press 'P' to convert pounds to kilograms.");
+            Console.WriteLine("Press 'M' to convert feet to meters.");
+
+            ConsoleKeyInfo keyinput = Console.ReadKey();
+
+            switch (keyinput.Key)
+            {
+                case ConsoleKey.F:
+                    Imperial.ToCelsius();
+                    break;
+                case ConsoleKey.P:
+                    Imperial.ToKilograms();
+                    break;
+                case ConsoleKey.M:
+                    Imperial.ToMeters();
+                    break;
+            }
+
+        }
+        
+    }
+    
+}
